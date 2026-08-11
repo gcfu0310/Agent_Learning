8d2942524b631e8f0d9c691b366831408481781d
gcfu0310 <gcfu0310@gmail.com>
2026-08-11 17:55:39 +0800

    修改输出逻辑，防止content为空值的情况

--- code/2026-08-11/04_openai_with_history.py
+++ code/2026-08-11/04_openai_with_history.py
@@ -28,8 +28,10 @@ for chunk in responses:
     # 有些API在最后会返回一个chunk，这个chunk.choices是一个[],需要添加一个判断条件，来判断是否能print
     # 否则这个时候用chunk.choices[0]会出现index out of range的问题。
     if chunk.choices:
-        print(
-            chunk.choices[0].delta.content, # delta很好理解，即数学中的增量Δ
-            end = '', # end='',代表不换行，末尾什么都没有。python的print默认是要换行的，即末尾end='\n'
-            flush=True # end="" 没有换行，输出可能不会立即刷新,而是储存在输出缓冲区里。flush=True 会强制马上写到终端，从而呈现流式效果。
-        )
+        content = chunk.choices[0].delta.content
+        if content:
+            print(
+                chunk.choices[0].delta.content, # delta很好理解，即数学中的增量Δ
+                end = '', # end='',代表不换行，末尾什么都没有。python的print默认是要换行的，即末尾end='\n'
+                flush=True # end="" 没有换行，输出可能不会立即刷新,而是储存在输出缓冲区里。flush=True 会强制马上写到终端，从而呈现流式效果。
+            )
