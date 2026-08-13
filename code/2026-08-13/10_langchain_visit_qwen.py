9a350a9c1f63c5690838f697deadbe8aafbf4666
gcfu0310 <gcfu0310@gmail.com>
2026-08-13 17:35:41 +0800

    langchain通过API调用大语言模型以及调用本地模型

---
+++ code/2026-08-13/10_langchain_visit_qwen.py
@@ -0,0 +1,21 @@
+from langchain_openai import ChatOpenAI
+from langchain_ollama import OllamaLLM
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# 调用服务器的LLMs
+llm = ChatOpenAI(
+    model="qwen-max",
+    api_key=os.getenv("DASHSCOPE_API_KEY"),
+    base_url=os.getenv("base_url")
+)
+res = llm.invoke("讲一个超级好笑的笑话逗笑我")
+print(res.content)
+
+# 调用本地的模型
+O_llm = OllamaLLM(
+    model="qwen2.5:7b"
+)
+res = O_llm.invoke("讲一个谐音梗笑话")
+print(res)
